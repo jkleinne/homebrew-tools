c26efa3698d16d969b924dee71ca1c55f5bbcf4f
jkleinne <jkq@jkleinne.com>
2026-03-14 00:06:25 -0400

    chore(formula): switch to git URL strategy for private repo

--- Formula/lazyspeed.rb
+++ Formula/lazyspeed.rb
@@ -1,9 +1,11 @@
 class Lazyspeed < Formula
   desc "Terminal-based internet speed test"
   homepage "https://github.com/jkleinne/lazyspeed"
-  url "https://github.com/jkleinne/lazyspeed/archive/refs/tags/v0.1.3-4.tar.gz"
-  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+  url "https://github.com/jkleinne/lazyspeed.git",
+      tag:      "v0.1.3-4",
+      revision: "40e587c7209d70486047249defbe34e7d412f937"
   license "MIT"
+  head "https://github.com/jkleinne/lazyspeed.git", branch: "main"
 
   depends_on "go" => :build
 
@@ -13,6 +15,6 @@ class Lazyspeed < Formula
   end
 
   test do
-    assert_match "version #{version}", shell_output("#{bin}/lazyspeed version")
+    assert_match "lazyspeed version", shell_output("#{bin}/lazyspeed version")
   end
 end
